01dbfbecc69dd3a08b0bf25133ce478627f83653
namanica <veronichkapilipna@gmail.com>
2024-04-17 16:02:09 +0300

    completed one part of the 4 lab

---
+++ asd-4lab-2sem/point-2/point-2-def.js
@@ -0,0 +1,216 @@
+const canvasDef = document.getElementById('defined-graph-box');
+const contextDef = canvasDef.getContext('2d');
+
+const nodePositionsDef = [
+    { x: 150, y: 150 },
+    { x: 300, y: 150 },
+    { x: 450, y: 150 },
+    { x: 600, y: 150 },
+    { x: 600, y: 350 },
+    { x: 600, y: 550 },
+    { x: 450, y: 550 },
+    { x: 300, y: 550 },
+    { x: 150, y: 550 },
+    { x: 150, y: 350 },
+    { x: 375, y: 350 }
+];
+const arrowPositionsDef = [
+    { x: 160, y: 125 },
+    { x: 310, y: 125 },
+    { x: 460, y: 125 },
+    { x: 610, y: 125 },
+    { x: 625, y: 365 },
+    { x: 585, y: 575 },
+    { x: 435, y: 575 },
+    { x: 285, y: 575 },
+    { x: 135, y: 575 },
+    { x: 125, y: 360 },
+    { x: 360, y: 375 }
+];
+
+const nodeNumberDef = nodePositionsDef.length;
+const kDef = 1 - 0.02 - 9 * 0.01 - 0.3;
+
+const drawGraphNodesDef = () => {
+    contextDef.fillStyle = 'white';
+
+    nodePositionsDef.forEach((nodePositionDef, index) => {
+        contextDef.beginPath();
+        contextDef.arc(nodePositionDef.x, nodePositionDef.y, 20, 0, Math.PI * 2, true);
+        contextDef.fill();
+        contextDef.stroke();
+    });
+
+    contextDef.font = '14px Arial';
+    contextDef.textAlign = 'center';
+    contextDef.textBaseline = 'middle';
+    contextDef.fillStyle = 'black';
+    nodePositionsDef.forEach((nodePositionDef, index) => {
+        contextDef.fillText(`${index + 1}`, nodePositionDef.x, nodePositionDef.y);
+    });
+};
+
+const generateAdjacencyMatrixDef = () => {
+    const matrix = [];
+    for (let i = 0; i < nodeNumberDef; i++) {
+        matrix[i] = [];
+        for (let j = 0; j < nodeNumberDef; j++) {
+            matrix[i][j] = Math.random() * 2 * kDef;
+            matrix[i][j] = matrix[i][j] < 1 ? 0 : 1;
+        }
+    }
+    return matrix;
+}
+
+const calculatePowersDef = (adjacencyMatrix) => {
+    let degrees = {};
+
+    for (let i = 0; i < nodeNumberDef; i++) {
+        let degPlus = 0;
+        let degMinus = 0;
+
+        for (let j = 0; j < nodeNumberDef; j++) {
+            if (adjacencyMatrix[j][i] === 1) {
+                degPlus++;
+            }
+            if (adjacencyMatrix[i][j] === 1) {
+                degMinus++;
+            }
+        }
+
+        degrees[i + 1] = { 'deg+': degPlus, 'deg-': degMinus };
+    }
+
+    return degrees;
+}
+
+const listHangIsoNodesDef = (powers) => {
+    const hangNodes = [];
+    const isoNodes = [];
+
+    Object.entries(powers).forEach(([node, power]) => {
+        const { 'deg+': degPlus, 'deg-': degMinus } = power;
+
+        if ((degPlus === 1 && degMinus === 0) || (degPlus === 0 && degMinus === 1)) {
+            hangNodes.push(node);
+        }
+        if (degPlus === 0 && degMinus === 0) {
+            isoNodes.push(node);
+        }
+    });
+
+    return {
+        'hanged nodes': hangNodes,
+        'isolated nodes': isoNodes,
+    };
+}
+
+const checkSimilarityDef = (powers) => {
+    let similarity = true;
+    let commonDeg = null;
+
+    for (let [node, power] of Object.entries(powers)) {
+        if (commonDeg === null) {
+            if (power['deg+'] === power['deg-']) {
+                commonDeg = power['deg+'];
+            } else {
+                similarity = false;
+                break;
+            }
+        } else {
+            if (power['deg+'] !== commonDeg || power['deg-'] !== commonDeg) {
+                similarity = false;
+                break;
+            }
+        }
+    }
+    return { similarity, commonDeg };
+}
+
+const calculateAdjustedStartPoint = (startX, startY, endX, endY, radius) => {
+    const angle = Math.atan2(endY - startY, endX - startX);
+
+    const adjustedX = startX + (radius * Math.cos(angle));
+    const adjustedY = startY + (radius * Math.sin(angle));
+
+    return { x: adjustedX, y: adjustedY };
+}
+
+const drawGraphEdgesDef = (adjacencyMatrix) => {
+    contextDef.strokeStyle = 'white';
+    contextDef.lineWidth = 1;
+
+    for (let i = 0; i < nodeNumberDef; i++) {
+        for (let j = 0; j < nodeNumberDef; j++) {
+
+            if (adjacencyMatrix[i][j] === 1) {
+                const startX = nodePositionsDef[j].x;
+                const startY = nodePositionsDef[j].y;
+
+                const endX = nodePositionsDef[i].x;
+                const endY = nodePositionsDef[i].y;
+
+                if (i === j) {
+                    contextDef.beginPath();
+                    if (i < 4) {
+                        contextDef.arc(endX,  (endY - 40), 20, -(Math.PI/2), 3*Math.PI/2, true);
+                        drawArrow(arrowPositionsDef[i].x, arrowPositionsDef[i].y, -(3*Math.PI / 13));
+                    } else if ((i > 4 && i < 9) || i === 10) {
+                        contextDef.arc(endX,  (endY + 40), 20, Math.PI/2, -(3*Math.PI/2), true);
+                        drawArrow(arrowPositionsDef[i].x, arrowPositionsDef[i].y, (3*Math.PI / 5));
+                    } else if (i === 9) {
+                        contextDef.arc((endX - 40),  endY, 20, 0, 2*Math.PI, true);
+                        drawArrow(arrowPositionsDef[i].x, arrowPositionsDef[i].y, (3*Math.PI / 5));
+                    } else {
+                        contextDef.arc((endX + 40),  endY, 20, Math.PI, -(Math.PI), true);
+                        drawArrow(arrowPositionsDef[i].x, arrowPositionsDef[i].y, (3*Math.PI / 11));
+                    }
+
+                    contextDef.stroke();
+                } else {
+                    contextDef.beginPath();
+                    contextDef.moveTo(startX, startY);
+                    contextDef.lineTo(endX, endY);
+
+                    let adjustedStartPoint = calculateAdjustedStartPoint(startX, startY, endX, endY, 20);
+                    const angle = Math.atan2(endY - startY, endX - startX);
+                    drawArrow(adjustedStartPoint.x, adjustedStartPoint.y, angle);
+                    contextDef.stroke();
+                }
+            }
+        }
+    }
+};
+
+const drawArrow = (x, y, angle) => {
+    const arrowSize = 6;
+    contextDef.save();
+    contextDef.translate(x, y);
+    contextDef.rotate(angle);
+    contextDef.moveTo(0, 0);
+    contextDef.lineTo(arrowSize, -arrowSize);
+    contextDef.lineTo(arrowSize / 2, 0);
+    contextDef.lineTo(arrowSize, arrowSize);
+    contextDef.closePath();
+    contextDef.restore();
+}
+
+if (contextDef) {
+    const adjacencyMatrix = generateAdjacencyMatrixDef();
+    console.log(adjacencyMatrix);
+
+    const powers = calculatePowersDef(adjacencyMatrix);
+    console.log("Power of each node in defined graph:", powers);
+    console.log("The lists of hanged and isolated nodes in defined graph:", listHangIsoNodesDef(powers));
+
+    const { similarity, commonDeg } = checkSimilarityDef(powers);
+    console.log('Is the defined graph similar?', similarity);
+    if (similarity) {
+        console.log('The similarity power of defined graph is:', commonDeg);
+    }
+
+    drawGraphEdgesDef(adjacencyMatrix);
+    drawGraphNodesDef();
+} else {
+    console.log('canvas are not supported');
+}
---
+++ asd-4lab-2sem/point-2/point-2-non-def.js
@@ -0,0 +1,172 @@
+const canvasNonDef = document.getElementById('non-defined-graph-box');
+const contextNonDef = canvasNonDef.getContext('2d');
+
+const nodePositionsNonDef = [
+    { x: 150, y: 150 },
+    { x: 300, y: 150 },
+    { x: 450, y: 150 },
+    { x: 600, y: 150 },
+    { x: 600, y: 350 },
+    { x: 600, y: 550 },
+    { x: 450, y: 550 },
+    { x: 300, y: 550 },
+    { x: 150, y: 550 },
+    { x: 150, y: 350 },
+    { x: 375, y: 350 }
+];
+
+const nodeNumberNonDef = nodePositionsNonDef.length;
+const kNonDef = 1 - 0.02 - 9 * 0.01 - 0.3;
+
+const drawGraphNodesNonDef = () => {
+    contextNonDef.fillStyle = 'white';
+
+    nodePositionsNonDef.forEach((nodePositionNonDef, index) => {
+        contextNonDef.beginPath();
+        contextNonDef.arc(nodePositionNonDef.x, nodePositionNonDef.y, 20, 0, Math.PI * 2, true);
+        contextNonDef.fill();
+        contextNonDef.stroke();
+    });
+
+    contextNonDef.font = '14px Arial';
+    contextNonDef.textAlign = 'center';
+    contextNonDef.textBaseline = 'middle';
+    contextNonDef.fillStyle = 'black';
+    nodePositionsNonDef.forEach((nodePositionNonDef, index) => {
+        contextNonDef.fillText(`${index + 1}`, nodePositionNonDef.x, nodePositionNonDef.y);
+    });
+};
+
+const generateAdjacencyMatrixNonDef = () => {
+    const matrix = [];
+    for (let i = 0; i < nodeNumberNonDef; i++) {
+        matrix[i] = [];
+        for (let j = 0; j < nodeNumberNonDef; j++) {
+            matrix[i][j] = Math.random() * 2 * kNonDef;
+            matrix[i][j] = matrix[i][j] < 1 ? 0 : 1;
+        }
+    }
+    return matrix;
+}
+
+const calculatePowersNonDef = (adjacencyMatrix) => {
+    const powers = new Array(nodeNumberNonDef).fill(0);
+
+    for (let i = 0; i < nodeNumberNonDef; i++) {
+        for (let j = 0; j < nodeNumberNonDef; j++) {
+            if (adjacencyMatrix[i][j] === 1) {
+                powers[i] += 1;
+                if (i !== j) {
+                    powers[j] += 1;
+                }
+            }
+        }
+    }
+
+    const powersObject = powers.reduce((acc, power, index) => {
+        acc[index + 1] = power;
+        return acc;
+    }, {});
+
+    return powersObject;
+}
+
+const listHangIsoNodesNonDef = (powers) => {
+    const hangNodes = [];
+    const isoNodes = [];
+
+    for (let nodeId in powers) {
+        if (powers[nodeId] === 1) {
+            hangNodes.push(nodeId);
+        } else if (powers[nodeId] === 0) {
+            isoNodes.push(nodeId);
+        }
+    }
+
+    return {
+        'hanged nodes' : hangNodes,
+        'isolated nodes': isoNodes,
+    };
+};
+
+const checkSimilarityNonDef = (powers) => {
+    const degrees = Object.values(powers);
+    const firstDeg = degrees[0];
+    return degrees.every(degree => degree === firstDeg) ? firstDeg : false;
+};
+
+const drawGraphEdgesNonDef = (adjacencyMatrix) => {
+    contextNonDef.strokeStyle = 'white';
+    contextNonDef.lineWidth = 1;
+
+    for (let i = 0; i < nodeNumberNonDef; i++) {
+        for (let j = 0; j < nodeNumberNonDef; j++) {
+
+            if (adjacencyMatrix[i][j] === 1) {
+                const startX = nodePositionsNonDef[j].x;
+                const startY = nodePositionsNonDef[j].y;
+                const endX = nodePositionsNonDef[i].x;
+
+                const endY = nodePositionsNonDef[i].y;
+                const midX = (startX + endX) / 2;
+
+                if (i === j) {
+                    contextNonDef.beginPath();
+                    if (i < 4) {
+                        contextNonDef.arc(endX, (endY - 35), 20, -(Math.PI/2),
+                            3*Math.PI/2, true);
+                    } else if ((i > 4 && i < 9) || i === 10) {
+                        contextNonDef.arc(endX, (endY + 35), 20, Math.PI/2, -
+                            (3*Math.PI/2), true);
+                    } else if (i === 9) {
+                        contextNonDef.arc((endX - 35), endY, 20, 0, 2*Math.PI,
+                            true);
+                    } else {
+                        contextNonDef.arc((endX + 35), endY, 20, Math.PI, -
+                            (Math.PI), true);
+                    }
+
+                    contextNonDef.stroke();
+                } else {
+                    contextNonDef.beginPath();
+                    if (i < 4) {
+                        contextNonDef.moveTo(startX, startY);
+                        contextNonDef.lineTo(midX, startY - 50);
+                        contextNonDef.lineTo(endX, endY);
+                    } else if ((i > 4) && (i < 9)) {
+                        contextNonDef.moveTo(startX, startY);
+                        contextNonDef.lineTo(startX + 50, startY + 50);
+                        contextNonDef.lineTo(endX, endY);
+                    } else {
+                        contextNonDef.moveTo(startX, startY);
+                        contextNonDef.lineTo(startX - 100, startY + 50);
+                        contextNonDef.lineTo(endX, endY);
+                    }
+
+                    contextNonDef.stroke();
+                }
+            }
+        }
+    }
+};
+
+if (contextNonDef) {
+    const adjacencyMatrix = generateAdjacencyMatrixNonDef();
+    console.log(adjacencyMatrix);
+
+    const powers = calculatePowersNonDef(adjacencyMatrix);
+    console.log("Power of each node in non-defined graph:", powers);
+    console.log("The list of hanged and isolated nodes in non-defined graph:", listHangIsoNodesNonDef(powers));
+
+    const similarity = checkSimilarityNonDef(powers);
+    if (similarity === false) {
+        console.log('Is the non-defined graph similar?', similarity);
+    } else {
+        console.log('The similarity power of non-defined graph is:', similarity);
+    }
+
+    drawGraphEdgesNonDef(adjacencyMatrix);
+    drawGraphNodesNonDef();
+} else {
+    console.log('canvas are not supported');
+}
